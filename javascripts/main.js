0ccb9c3c437b93aadcf9cc07054dce54a235ad0e
Matias Saggiorato <matiassaggiorato@gmail.com>
2014-02-22 19:38:02 -0300

    Updated library and description.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,13 @@
 (function($){
-
 	$("a.stylify").click(function(e){
 		e.preventDefault();
-		$("select.cool-it").QuickSelect();
+		$("select.cool-it").QuickSelect({
+			openEffect: function(instance){
+				$(this).slideDown(200);
+			},
+			closeEffect: function(instance){
+				$(this).slideUp(200);
+			}
+		});
 	})
 })(jQuery)

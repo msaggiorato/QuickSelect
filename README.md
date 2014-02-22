14ca4f870109e39baf59e386624fff6d1da82711
Matias Saggiorato <matiassaggiorato@gmail.com>
2014-02-22 05:06:25 -0300

    Initial Push

--- README.md
+++ README.md
@@ -1,4 +1,83 @@
 QuickSelect
-===========
+=================
 
-Quick jQuery select tag replacement.
+A *Quick* replacement for the good old *Select* tags.
+
+Why I built this plugin?
+------------------
+
+This should be useful if you need to style some select box, without having extra headaches to worry about.
+
+I hope you like this!
+
+Why should you use this instead of another plugin?
+-------------------------------------------
+
+* I try to keep this as small and lightweight as possible.
+* I keep this as responsive as possible.
+* It works with events tied to the original select box.
+* It let's you stylize almost the entire selectbox layout.
+ 
+Optional parameters
+-------------------
+
+QuickSelect supports optional parameters.
+
+### iconUp
+The icon that will be displayed when the dropdown is visible.  
+**Default: "&#9650;"**
+
+### iconDown
+The icon that will be displayed when the dropdown is not visible.  
+**Default: "&#9660;"**
+
+### minElemsBeforeGoingUp
+This is the minimun number of options to display below the select box before trying to display it above the box. (You'll get this better if you play with it yourself.)
+**Default: 3**
+
+How to Use
+----------
+
+If you don't want to write any extra JS code, just add the *quick-select* class to your select box, and the plugin will automatically initialize.
+
+
+If you have something like this in your HTML.
+```html
+<select id="myCoolSelect" name="data">
+	<option value="1">This is an option</option>
+	<option value="2">And this is another</option>
+	<option value="3">Here comes the boom</option>
+	<option value="4">Kaboom!</option>
+</select>
+```
+
+Just do this with jQuery and you'll be good to go.
+```javascript
+$('#myCoolSelect').QuickSelect();
+```
+
+Maybe using some FontAwesome icons as an option?
+```javascript
+$('#myCoolSelect').QuickSelect({
+	iconDown: '<i class="icon-collapse"></i>',
+	iconUp:'<i class="icon-collapse-top"></i>'
+});
+```
+
+IE Support
+----------
+
+I haven't tested this on IE, but should work from IE8 and up. I'll update this later.
+
+License
+-------
+
+The MIT License (MIT) Copyright (c) 2014 Matias Saggiorato.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+**It means, you can freely fork and modify this project for commercial or non-comercial use!**
---
+++ jquery.quickSelect.js
@@ -0,0 +1,245 @@
+(function($){
+	$.fn.onResize = function(callback){
+		return $(this).each(function(){
+			var thisObj = this;
+			var thisElem = $(this);
+			var timer;
+			var resetTimer;
+			var check;
+
+			var lastWidth = thisElem.width(),
+				lastHeight = thisElem.height();
+
+			check = function(){
+				if(thisElem.width() != lastWidth || thisElem.height() != lastHeight){
+					lastWidth = thisElem.width(),
+					lastHeight = thisElem.height();
+					callback.call(thisObj);
+				}
+				resetTimer();
+			}
+
+			resetTimer = function(){
+				clearTimeout(timer);
+				timer = setTimeout(check, 50);
+			}
+			resetTimer();
+
+			$(window).resize(check);
+		});
+	}
+	$.fn.QuickSelect = function(options){
+		options = $.extend(true, {
+			iconUp: "&#9650;",
+			iconDown: "&#9660;",
+			minElemsBeforeGoingUp: 3
+		}, options);
+		$(this).filter("select").each(function(){
+			var thisElem = $(this);
+			thisElem.hide();
+			var main = $("<div class='qs'>").insertAfter(thisElem);
+			var fixed = $("<div class='qs-fixed'>").appendTo(main);
+			var dropdown = $("<div class='qs-dropdown'>").hide().appendTo(main);
+
+			//var viewportArrow = $("<table class='qs-viewport-arrow' cellspacing='0' cellpadding='0'><tr><td class='qs-viewport-container'><div class='qs-viewport-sub-cont'><div class='qs-viewport'></div></div></td><td class='qs-arrow'></td></tr></table>").appendTo(fixed);
+			var viewportArrow = $("<div class='qs-viewport-arrow'><div class='qs-arrow'></div><div class='qs-viewport-container'><div class='qs-viewport'></div></div></div>").appendTo(fixed);
+			var viewport = viewportArrow.find(".qs-viewport");
+			var arrow = viewportArrow.find(".qs-arrow");
+			var viewportContainer = viewportArrow.find(".qs-viewport-container");
+
+			viewportContainer.css({
+				marginRight: arrow.outerWidth(true)+"px"
+			});
+
+			arrow.html(options.iconDown);
+
+			var thisOptions = thisElem.children('option');
+
+
+			var getText = function(option){
+				var text = option.text();
+				if(text.length == 0) text = "&nbsp;";
+				return text;
+			}
+			var getSelected = function(){
+				var selected = thisOptions.filter(function(){
+					return $(this).prop("selected") === true;
+				});
+				if(selected.length === 0){
+					selected = thisOptions.first().prop("selected", true);
+				}
+				return selected;
+			}
+			var setSelected = function(opt, trigger){
+				if(typeof trigger === "undefined") trigger = true;
+				
+				thisOptions.each(function(){
+					if($(this).is(opt)){
+						if($(this).prop("selected") === true) trigger = false;
+						$(this).prop("selected", true);
+						$(this).data("qs-mirror").addClass('selected').siblings().removeClass('selected');
+						viewport.html(getText(opt));
+					} else {
+						$(this).prop("selected", false);
+					}
+				});
+				
+				if(trigger) thisElem.trigger("change", [true]);
+			}
+			var testSelected = function(text){
+				var oldW = viewport.width();
+				var oldText = viewport.html();
+				viewport.css("width", "");
+				viewport.html(text);
+				if(oldW < viewport.width()) {
+					viewport.width(viewport.width());
+				} else {
+					viewport.width(oldW);
+				}
+				viewport.html(oldText);
+			}
+			var setHover = function(option){
+				option.addClass('hover').siblings().removeClass('hover');
+			}
+
+			var getFirstOptionsHeight = function(){
+				var height = 0;
+				dropdown.children(".qs-option").slice( 0, options.minElemsBeforeGoingUp ).each(function(){
+					height += $(this).outerHeight(true);
+				});
+				return height;
+			}
+
+			var heightAsManyOptionsAsPossible = function(heightAvailable){
+				var height = 0;
+				dropdown.children(".qs-option").each(function(){
+					var thisHeight = $(this).outerHeight(true);
+					if(thisHeight + height < heightAvailable){
+						height += thisHeight;
+					}
+				});
+				return height;
+			}
+
+			var checkSize = function(){
+				
+				var viewportHeight = $(window).height();
+				var offset = main.offset();
+				var scrollTop = $(window).scrollTop();
+				var height = main.height();
+
+				var spaceBelow = viewportHeight - offset.top - height + scrollTop;
+				var spaceAbove = offset.top - scrollTop;
+
+				main.removeClass('open-above');
+				if(dropdown.is(":visible")){
+					dropdown.css("max-height", "");
+					var dropdownHeight = dropdown.outerHeight();
+					if(dropdownHeight > spaceBelow) {
+						var maxHeight = getFirstOptionsHeight();
+						if(spaceBelow >= maxHeight || spaceBelow >= spaceAbove){
+							dropdown.css("max-height", heightAsManyOptionsAsPossible(spaceBelow)+"px");
+						} else if(spaceAbove > spaceBelow) {
+							main.addClass('open-above');
+							dropdown.css("max-height", heightAsManyOptionsAsPossible(spaceAbove)+"px");
+						}
+					}
+				} else {
+					if(150 > spaceBelow) {
+						main.addClass('open-above');
+					}
+				}
+			}
+
+			var doDropdown = function(){
+				if(dropdown.is(":visible")){
+					hideDropdown();
+				} else {
+					setHover(getSelected().data("qs-mirror"));
+					dropdown.show();
+
+					checkSize();
+
+					dropdown.hide();
+
+
+					dropdown.fadeIn(100);
+					main.addClass('opened');
+				}
+			}
+			var hideDropdown = function(){
+				dropdown.hide();
+				main.removeClass('opened');
+			}
+			$(window).on("resize", function(){
+				checkSize();
+			});
+			$(window).on("scroll", function(){
+				checkSize();
+			});
+			fixed.on("click", function(){
+				doDropdown();
+			});
+
+			//detect click outside
+			$(document).on("click", function(e){
+				var target = $(e.target);
+				var matchingParents = target.parents().filter(function(){
+					return $(this).is(main);
+				}).length;
+				if(!target.is(main) && matchingParents == 0){
+					hideDropdown();
+				}
+			});
+
+			thisOptions.each(function(){
+				var thisOpt = $(this);
+				var optVal = thisOpt.attr("value");
+				var option = $("<div class='qs-option'>").appendTo(dropdown);
+				thisOpt.data("qs-mirror", option);
+
+				option.html(getText(thisOpt));
+
+				option.attr("data-value", optVal);
+				option.data("value", optVal);
+
+				option.on("mouseenter", function(){
+					setHover($(this));
+				});
+
+				testSelected(getText(thisOpt));
+
+				if(thisOpt.prop("selected") === true){
+					setSelected(thisOpt, false);
+				}
+
+				option.on("click", function(){
+					setSelected(thisOpt);
+					hideDropdown();
+				});
+			});
+
+			thisElem.on("change", function(e, hasToIgnore){
+				if(typeof hasToIgnore !== "undefined") return;
+				setSelected(getSelected());
+			});
+
+
+			var adjust = function(){
+				thisOptions.each(function(){
+					testSelected(getText($(this)));
+				});
+			};
+			main.onResize(adjust);
+			$(window).one("load", adjust);
+
+
+
+
+		});
+		return $(this);
+	}
+	$(function(){
+		$("select.quick-select").QuickSelect();
+	})
+})(jQuery);
